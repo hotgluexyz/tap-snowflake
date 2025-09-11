f4b3b92f8af58eb5cad83c8cc4389a367714fb45
Key <keynarafaelp@gmail.com>
2025-09-11 18:06:24 -0500

    accept variant type

--- tap_snowflake/__init__.py
+++ tap_snowflake/__init__.py
@@ -77,7 +77,7 @@ def schema_for_column(c):
     elif data_type in NUMBER_TYPES:
         result.type = ['null', 'number']
 
-    elif data_type in STRING_TYPES or data_type in ["array", "object"]:
+    elif data_type in STRING_TYPES or data_type in ["array", "object", "variant"]:
         result.type = ['null', 'string']
         # result.maxLength = c.character_maximum_length
 
