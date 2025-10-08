ed9af46df9e5aba74c099e7ae94d4edbb77ba216
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2025-10-08 08:19:34 -0500

    HGI-8963: Fix snowflake connector version for 3.7 (#14)
    * fix snowflake connector version for 3.7
    
    * add azure-storage-blob to requirements for 3.7

--- setup.py
+++ setup.py
@@ -23,8 +23,9 @@ setup(name='pipelinewise-tap-snowflake',
             'pyOpenSSL==22.1.0',
             'pipelinewise-singer-python==1.*',
             'requests==2.22.0',
-            'snowflake-connector-python~=2.0.4; python_version == "3.7"',
+            'snowflake-connector-python==2.0.2; python_version == "3.7"',
             'snowflake-connector-python~=3.4.0; python_version == "3.10"',
+            'azure-storage-blob<2.0.0; python_version == "3.7"',
             'pendulum==1.2.0'
       ],
       extras_require={
